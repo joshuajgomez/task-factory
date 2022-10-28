9a80ef0a1ebe7ee16e21bed718dc81ae4b9502f2
Joshua Gomez <joshua.gomez@quest-global.com>
2022-10-28 16:42:05 +0900

    Initial commit

---
+++ app/src/main/java/com/joshgm3z/taskfactory/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package com.joshgm3z.taskfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
