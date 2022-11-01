3dc847d6740bd7f1e1a58a5f153e77e98b8ecd1d
Joshua Gomez <joshua.gomez@quest-global.com>
2022-11-01 14:52:08 +0900

    Create base layout with ConstraintLayout

--- app/src/main/java/com/joshgm3z/taskfactory/DashboardActivity.kt
+++ app/src/main/java/com/joshgm3z/taskfactory/DashboardActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        setContentView(R.layout.activity_dashboard)
     }
 }
